aa23f375eb7b0f1648cccce604037d19536862df
reamted <nroussos2028@kehillahstudent.org>
2025-04-30 12:17:48 -0700

    that took longer then expected

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import json
 
 # read `expenses.json`
+with open("expenses.json", "r") as pet_store:
+    whole_thing = json.load(pet_store)
 
 # get and print total "price" for all expenses at the "pet store"
+
+#stuff = whole_thing["pet store"]
+#print(stuff)
+total = 0
+something = whole_thing["pet store"]
+for stuff in something:
+    total += stuff["price"]
+print(total)
